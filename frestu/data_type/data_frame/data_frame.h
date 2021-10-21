bb71a8e6fbccf8020b2ec35fdfed850ba99996c0
vagrant <vagrant@vagrant.vm>
2021-10-21 00:16:38 +0000

    漸化式を解けるようにした

--- frestu/data_type/data_frame/data_frame.h
+++ frestu/data_type/data_frame/data_frame.h
@@ -13,23 +13,23 @@ using Int = int;
 using std::get;
 using std::index_sequence;
 using std::make_index_sequence;
+using std::max;
 using std::tuple_size;
 using Eigen::Dynamic;
 using Eigen::Matrix;
 using Eigen::Vector;
 
-template <typename Columns>
+template <typename DfColumns>
 class DataFrame {
-  static constexpr size_t size_columns_ = tuple_size<Columns>::value;
+  static constexpr size_t size_columns_ = tuple_size<DfColumns>::value;
   using ixs_column = make_index_sequence<size_columns_>;
 
   const Int alternative_to_nan_;
   Int n_rows_;
-  Int n_columns_;
 
   template <typename FnRet, typename Fn, size_t... ixs>
-  Vector<FnRet, Dynamic> Apply(Fn func, index_sequence<ixs...>) {
-    Vector<FnRet, Dynamic> vec(n_columns_);
+  Vector<FnRet, Dynamic> Apply(const Fn& func, index_sequence<ixs...>) {
+    Vector<FnRet, Dynamic> vec(size_columns_);
 
     using swallow = int[];
     (void)swallow {
@@ -41,15 +41,18 @@ class DataFrame {
 
   template <typename FnRet, typename Fn, size_t... ixs>
   Vector<FnRet, Dynamic> Apply(
-      Fn func, Int first, Int last, index_sequence<ixs...>) {
-    Vector<FnRet, Dynamic> vec(n_columns_);
+      const Fn& func, const Int first, const Int last, index_sequence<ixs...>) {
+    Vector<FnRet, Dynamic> vec(size_columns_);
 
     using swallow = int[];
     (void)swallow {
       (
         [&]() {
-          auto column = get<ixs>(columns_);
-          vec[ixs] = func(column.block(first, 0, last - first, column.cols()));
+          // データフレームのカラム
+          const auto column = get<ixs>(columns_);
+          const size_t n_cols = column.cols();
+          const auto b = column.block(first, 0, last - first, n_cols);
+          vec[ixs] = func(b);
         }(),
         0
       )...
@@ -60,25 +63,33 @@ class DataFrame {
 
   template <typename FnRet, typename Fn, size_t... ixs>
   Matrix<FnRet, Dynamic, size_columns_> Roll(
-      Fn func, Int window, index_sequence<ixs...>) {
-    Matrix<FnRet, Dynamic, Dynamic> mat(n_rows_, n_columns_);
+      const Fn& func, const Int window, index_sequence<ixs...>) {
+    Matrix<FnRet, Dynamic, Dynamic> mat(n_rows_, size_columns_);
+
+    // ブロック生成時に使う、行のオフセット
+    const Int offset = window - 1;
+    
+    // NaN である行数
+    const Int n_nans = window - 1;
 
     using swallow = int[];
     (void)swallow {
       (
         [&]() {
-          auto column = get<ixs>(columns_);
+          // データフレームのカラム
+          const auto column = get<ixs>(columns_);
+          // データフレームのカラムを構成するベクトルのカラム数
+          const size_t n_cols = column.cols();
 
           // 計算できない行に alternative_to_nan_ を入れる。
-          Int n_nans = window - 1;
           for (Int i_row = 0; i_row < n_nans; i_row++) {
             mat(i_row, ixs) = static_cast<FnRet>(alternative_to_nan_);
           }
 
           // ローリング
           for (Int i_row = n_nans; i_row < n_rows_; i_row++) {
-            mat(i_row, ixs) = func(column.block(
-              i_row - n_nans, 0, window, column.cols()));
+            const auto b = column.block(i_row - offset, 0, window, n_cols);
+            mat(i_row, ixs) = func(b);
           }
         }(),
         0
@@ -88,28 +99,90 @@ class DataFrame {
     return mat;
   };
 
-public:
-  Columns columns_;
+  template <typename FnRet, typename Fn, size_t... ixs>
+  Matrix<FnRet, Dynamic, size_columns_> RollRecursion(
+      const Fn& func, const Int window, const Int window_recursive,
+      const Matrix<FnRet, Dynamic, size_columns_>& initial_values,
+      index_sequence<ixs...>) {
+    Matrix<FnRet, Dynamic, Dynamic> mat(n_rows_, size_columns_);
 
-  DataFrame(Columns& columns, Int alternative_to_nan=9999)
-      : alternative_to_nan_(alternative_to_nan), columns_(columns) {
-    n_rows_ = get<0>(columns_).rows();
-    n_columns_ = size_columns_;
+    // ブロック生成時に使う、行のオフセット
+    const Int offset = window - 1;
+    // 一つ前の値からなので -1 を付けない。
+    const Int offset_recursive = window_recursive;
+
+    // NaN である行数
+    const Int window_max = max(window, window_recursive);
+    const Int n_nans = window_max - 1;
+
+    using swallow = int[];
+    (void)swallow {
+      (
+        [&]() {
+          // データフレームのカラム
+          const auto column = get<ixs>(columns_);
+          // データフレームのカラムを構成するベクトルのカラム数
+          const size_t n_cols = column.cols();
+
+          // 計算できない行に alternative_to_nan_ を入れる。
+          for (Int i_row = 0; i_row < n_nans; i_row++) {
+            mat(i_row, ixs) = static_cast<FnRet>(alternative_to_nan_);
+          }
+
+          // 初期計算
+          const auto initial_b = column.block(0, 0, window, n_cols);
+          const auto initial_values_recursive = initial_values.block(
+            0, ixs, window_recursive, 1);
+          mat(n_nans, ixs) = func(initial_b, initial_values_recursive);
+
+          // 再帰ローリング
+          for (Int i_row = window_max; i_row < n_rows_; i_row++) {
+            const auto b = column.block(i_row - offset, 0, window, n_cols);
+            const auto values_recursive = mat.block(
+              i_row - offset_recursive, ixs, window_recursive, 1);
+            mat(i_row, ixs) = func(b, values_recursive);
+          }
+        }(),
+        0
+      )...
+    };
+
+    return mat;    
   };
 
+public:
+  const DfColumns columns_;
+
+  DataFrame(DfColumns& columns, Int alternative_to_nan=9999)
+      : alternative_to_nan_(alternative_to_nan), columns_(columns) {
+    n_rows_ = get<0>(columns_).rows();
+  };
+
+  DataFrame(DfColumns&& columns, Int alternative_to_nan=9999)
+      : DataFrame(columns, alternative_to_nan) {};
+
   template <typename FnRet, typename Fn>
-  Vector<FnRet, Dynamic> Apply(Fn func) {
+  Vector<FnRet, Dynamic> Apply(const Fn& func) {
     return Apply<FnRet>(func, ixs_column());
   };
 
   template <typename FnRet, typename Fn>
-  Vector<FnRet, Dynamic> Apply(Fn func, Int first, Int last) {
+  Vector<FnRet, Dynamic> Apply(
+      const Fn& func, const Int first, const Int last) {
     return Apply<FnRet>(func, first, last, ixs_column());
   };
 
   template <typename FnRet, typename Fn>
-  Matrix<FnRet, Dynamic, size_columns_> Roll(Fn func, Int window) {
+  Matrix<FnRet, Dynamic, size_columns_> Roll(const Fn& func, const Int window) {
     return Roll<FnRet>(func, window, ixs_column());
+  };
+
+  template <typename FnRet, typename Fn>
+  Matrix<FnRet, Dynamic, size_columns_> RollRecursion(
+      const Fn& func, const Int window, const Int window_recursive,
+      const Matrix<FnRet, Dynamic, size_columns_>& initial_values) {
+    return RollRecursion<FnRet>(
+      func, window, window_recursive, initial_values, ixs_column());
   };
 };
 
--- frestu/test/data_type/data_frame/data_frame.cc
+++ frestu/test/data_type/data_frame/data_frame.cc
@@ -16,12 +16,12 @@ using std::endl;
 using std::get;
 using std::make_tuple;
 using std::string;
+using std::tuple_size;
 using std::vector;
 using Eigen::Dynamic;
 using Eigen::Map;
 using Eigen::Matrix;
 using Eigen::MatrixXd;
-using Eigen::Vector;
 using Eigen::VectorXi;
 using Eigen::VectorXd;
 using frestu::data_type::data_frame::DataFrame;
@@ -30,61 +30,95 @@ using frestu::util::io::TableReader;
 auto tr = TableReader("./data/ts.dat");
 // time
 auto indices = tr.ReadColumn<string>(0, [](auto v){return v;});
-auto col_i = tr.ReadColumn<Int>(1, [](auto v){return stoi(v);});
+auto value_i = tr.ReadColumn<Int>(1, [](auto v){return stoi(v);});
 
-auto ixs_cols_d = vector<int>{1, 2};
-auto cols_d = tr.ReadColumns<Real>(ixs_cols_d, [](auto v){return stod(v);});
+auto ixs_d = vector<int>{1, 2};
+auto value_d = tr.ReadColumns<Real>(ixs_d, [](auto v){return stod(v);});
 
-VectorXi vec_i = Map<VectorXi>(&col_i[0], col_i.size());
-MatrixXd mat_d = Map<MatrixXd>(&cols_d[0], tr.n_rows(), ixs_cols_d.size());
+VectorXi vec_i = Map<VectorXi>(&value_i[0], value_i.size());
+MatrixXd mat_d = Map<MatrixXd>(&value_d[0], tr.n_rows(), ixs_d.size());
 
-auto columns = make_tuple(vec_i, mat_d);
-auto df = DataFrame(columns);
+auto df_columns = make_tuple(vec_i, mat_d);
+auto df = DataFrame(df_columns);
 
 TEST(DataFrame, Apply_Fn) {
-  auto sum_df = df.Apply<Real>([](auto c){return c.sum();});
+  auto sum_df = df.Apply<Real>([](auto& b){return b.sum();});
 
+  // vec_i
   EXPECT_NEAR(sum_df[0], 275, 0.01);
+  // mat_d
   EXPECT_NEAR(sum_df[1], 551.1, 0.01);
 }
 
 TEST(DataFrame, Apply_Fn_Int_Int) {
-  auto sum_1 = df.Apply<Real>([](auto c){return c.sum();}, 0, 0);
-  auto sum_2 = df.Apply<Real>([](auto c){return c.sum();}, 0, 1);
-  auto sum_3 = df.Apply<Real>([](auto c){return c.sum();}, 0, 5);
-  auto sum_4 = df.Apply<Real>([](auto c){return c.sum();}, 3, 6);
+  auto sum_1 = df.Apply<Real>([](auto& b){return b.sum();}, 0, 0);
+  auto sum_2 = df.Apply<Real>([](auto& b){return b.sum();}, 0, 1);
+  auto sum_3 = df.Apply<Real>([](auto& b){return b.sum();}, 0, 5);
+  auto sum_4 = df.Apply<Real>([](auto& b){return b.sum();}, 3, 6);
 
+  // vec_i
   EXPECT_NEAR(sum_1[0], 0.0, 0.01);
-  EXPECT_NEAR(sum_1[1], 0.0, 0.01);
   EXPECT_NEAR(sum_2[0], 0.0, 0.01);
-  EXPECT_NEAR(sum_2[1], 0.1, 0.01);
   EXPECT_NEAR(sum_3[0], 85, 0.01);
-  EXPECT_NEAR(sum_3[1], 170.5, 0.01);
   EXPECT_NEAR(sum_4[0], 70, 0.01);
+
+  // mat_d
+  EXPECT_NEAR(sum_1[1], 0.0, 0.01);
+  EXPECT_NEAR(sum_2[1], 0.1, 0.01);
+  EXPECT_NEAR(sum_3[1], 170.5, 0.01);
   EXPECT_NEAR(sum_4[1], 140.3, 0.01);
 }
 
 TEST(DataFrame, Roll) {
-  auto roll_1 = df.Roll<Real>([](auto w){return w.mean();}, 2);
-  auto roll_2 = df.Roll<Real>([](auto w){return w.mean();}, 3);
+  auto roll_1 = df.Roll<Real>([](auto& b){return b.mean();}, 2);
+  auto roll_2 = df.Roll<Real>([](auto& b){return b.mean();}, 3);
 
+  // vec_i
   EXPECT_NEAR(roll_1(0, 0), 9999, 0.01);
   EXPECT_NEAR(roll_1(1, 0), 5, 0.01);
   EXPECT_NEAR(roll_1(10, 0), 65, 0.01);
 
+  // mat_d
   EXPECT_NEAR(roll_1(0, 1), 9999, 0.01);
   EXPECT_NEAR(roll_1(1, 1), 5.05, 0.01);
   EXPECT_NEAR(roll_1(10, 1), 65.05, 0.01);
 
+  // vec_i
   EXPECT_NEAR(roll_2(0, 0), 9999, 0.01);
   EXPECT_NEAR(roll_2(1, 0), 9999, 0.01);
   EXPECT_NEAR(roll_2(2, 0), 10, 0.01);
   EXPECT_NEAR(roll_2(10, 0), 53, 0.01);
 
+  // mat_d
   EXPECT_NEAR(roll_2(0, 1), 9999, 0.01);
   EXPECT_NEAR(roll_2(1, 1), 9999, 0.01);
   EXPECT_NEAR(roll_2(2, 1), 10.05, 0.01);
   EXPECT_NEAR(roll_2(10, 1), 53.38, 0.01);
+}
+
+TEST(DataFrame, Recurse) {
+  // 累積和
+  const Int window = 1;
+  const Int window_recursive = 1;
+  const size_t size_df_columns = 2; //tuple_size<columns>::value;
+
+  Matrix<Real, window_recursive, size_df_columns> initial_values;
+  initial_values(0, 0) = 0.0;
+  initial_values(0, 1) = 0.0;
+
+  auto recursion = df.RollRecursion<Real>([](auto& b, auto& r) {
+    return b.sum() + r.sum();
+  }, window, window_recursive, initial_values);
+
+  // vec_i
+  EXPECT_EQ(recursion(0, 0), 0);
+  EXPECT_EQ(recursion(1, 0), 10);
+  EXPECT_EQ(recursion(10, 0), 275);
+
+  // mat_d
+  EXPECT_NEAR(recursion(0, 1), 0.1, 0.1);
+  EXPECT_NEAR(recursion(1, 1), 20.2, 0.1);
+  EXPECT_NEAR(recursion(10, 1), 551.1, 0.1);
 }
 
 }
